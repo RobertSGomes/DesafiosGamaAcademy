28762d9645892cba3a46ed43010baba90349ac97
RobertSGomes <robertsg.dev@gmail.com>
2022-08-19 20:12:22 -0300

    Resolvendo os desafios

---
+++ index.js
@@ -0,0 +1,31 @@
+function desafio3() {
+    const num = prompt("Digite um número")
+
+    if (isNaN(num)) {
+        console.log("Não é um número")
+        desafio3()
+    } else {
+        num % 2 === 0 ? console.log("É par") : console.log("É impar")
+    }
+}
+
+function desafio4() {
+    const param = prompt("Digite um parâmetro")
+
+    for (let i = param; i > 0; i--) {
+        var array_asteristicos = [],
+            asteristicos = ""
+
+        for (let j = 0; j < i; j++) {
+            array_asteristicos.push("*")
+        }
+
+        for (let j = 0; j < i; j++) {
+            asteristicos += array_asteristicos[j]
+        }
+
+        console.log(asteristicos + "\n")
+    }
+}
+
+desafio4()
